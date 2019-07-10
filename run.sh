603f7ce6dd01ca16b013f696a469a29a5913923c
librevn <52727042+librevn@users.noreply.github.com>
2019-07-10 11:06:58 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -20,7 +20,12 @@ docker stop ${CONTAINER_NAME} > /dev/null
 # there is an option where we want to test container, by input '/bin/bast' letting
 # us to access to container shell (instead of start cloud9 starting script)
 if [ -z "$1" ]; then
-	docker run --rm -d ${VOLUME_OPT} ${PORT_OPT} --name ${CONTAINER_NAME} cloud9:latest
+	if [ ! -d ${SHARED_DIR}/work/cloud9 ]
+		echo "=== install cloud9, once it'd done, it will be possible to run as daemon"
+		docker run --rm -it ${VOLUME_OPT} ${PORT_OPT} --name ${CONTAINER_NAME} cloud9:latest /usr/local/bin/start-cloud9
+	else
+		docker run --rm -d ${VOLUME_OPT} ${PORT_OPT} --name ${CONTAINER_NAME} cloud9:latest
+	fi
 else
 	docker run --rm -it ${VOLUME_OPT} ${PORT_OPT} --name ${CONTAINER_NAME} cloud9:latest $*
 fi

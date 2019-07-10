afc4382b5fe168f48092b10ea65097305d149381
S Dao <dao@singoc.localdomain>
2019-07-10 10:04:02 +0700

    Update start script

--- builder/start-ide.sh
+++ builder/start-ide.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 C9SDK_DIR=/work/cloud9
-C9SDK_INSTALL_CMD=${C9SDK_DIR}/scripts/install-sdk.sh
-USERNAME=$(egrep username < auth.conf | head -n 1 | awk -F: '{print $2}')
-PASSWORD=$(egrep password < auth.conf | tail -n 1 | awk -F: '{print $2}')
+TOOLS_INSTALL_CMD="C9SDK_INSTALL_CMD=${C9SDK_DIR}/scripts/install-sdk.sh"
+C9SDK_INSTALL_CMD="${TOOLS_INSTALL_CMD} && ${C9SDK_DIR}/scripts/install-sdk.sh"
 
 export PATH=~/.c9/node/bin:/home/user/.c9/node/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games
 
@@ -15,13 +14,9 @@ else
 fi
 
 if [ ! -d ${C9SDK_DIR} ]; then
-        git --version >> /dev/null
-        if [ $? != 0 ]; then
-		echo "=== install required tools"
-                sudo apt-get install git wget python2.7 build-essential -y >> /dev/null
-        fi
-	
-	echo "=== clone cloud9-sdk"
+        ${TOOLS_INSTALL_CMD}
+
+        echo "=== clone cloud9-sdk"
         git clone https://github.com/c9/core.git ${C9SDK_DIR} >> /dev/null
 fi
 
@@ -40,8 +35,8 @@ if [ $? != 0 ]; then
         ${C9SDK_INSTALL_CMD}
 fi
 
-sed -i "s/auth\[0\]/\"${USERNAME}\"/g" ${C9SDK_DIR}/configs/standalone.js
-sed -i "s/auth\[1\]/\"${PASSWORD}\"/g" ${C9SDK_DIR}/configs/standalone.js
+sed -i "s/auth\[0\]/\"user\"/g" ${C9SDK_DIR}/configs/standalone.js
+sed -i "s/auth\[1\]/\"Pass1234\!\@\#\$\"/g" ${C9SDK_DIR}/configs/standalone.js
 
 if [ $? == 0 ]; then
 	echo "=== start server"
